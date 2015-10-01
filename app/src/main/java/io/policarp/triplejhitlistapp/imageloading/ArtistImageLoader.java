b0e44343ca6af5f63bc955d68185d2dfe8f1ab3d
Sean Policarpio <kdrakon@gmail.com>
2015-09-30 23:37:06 -0400

    removing some dead code

--- app/src/main/java/io/policarp/triplejhitlistapp/imageloading/ArtistImageLoader.java
+++ app/src/main/java/io/policarp/triplejhitlistapp/imageloading/ArtistImageLoader.java
@@ -1,7 +1,6 @@
 package io.policarp.triplejhitlistapp.imageloading;
 
 import java.io.ByteArrayOutputStream;
-import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
 
 import android.content.Context;
@@ -67,32 +66,6 @@ public class ArtistImageLoader implements Provider<ImageLoader>
         };
 
         return new ImageLoader(networkImageLoaderRequestQueue, imageCache);
-    }
-
-    /**
-     * from http://developer.android.com/training/displaying-bitmaps/load-bitmap.html
-     */
-    private static int calculateInSampleSize(
-            BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
     }
 
     private static class ArtistImageDiskCacheEntry extends com.android.volley.Cache.Entry
